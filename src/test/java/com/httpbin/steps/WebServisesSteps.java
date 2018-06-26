365be471dffed1e88b55b5eb11f85e66a1b47fcb
Srinivas Thummala <Srinivas.Thummala@infostretch.com>
2018-06-26 12:28:37 +0530

    commit

--- src/test/java/com/httpbin/steps/WebServisesSteps.java
+++ src/test/java/com/httpbin/steps/WebServisesSteps.java
@@ -4,7 +4,6 @@ import javax.ws.rs.core.MediaType;
 
 import org.hamcrest.Matchers;
 import org.json.JSONObject;
-import org.testng.annotations.Test;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -21,29 +20,27 @@ import com.sun.jersey.api.client.ClientResponse;
 
 public class WebServisesSteps extends RestWSTestCase {
 
-	
-	@QAFTestStep(description="user navigates to httpbin homepage")
-	public void userNavigatesToHttpbinHomepage(){
+	@QAFTestStep(description = "user navigates to httpbin homepage")
+	public void userNavigatesToHttpbinHomepage() {
 		HomeTestPage homeTestPage = new HomeTestPage();
 		homeTestPage.launchPage(null);
 	}
-	
-	@QAFTestStep(description="user clicks html form")
-	public void userClicksHtmlForm(){
+
+	@QAFTestStep(description = "user clicks html form")
+	public void userClicksHtmlForm() {
 		HomeTestPage homeTestPage = new HomeTestPage();
 		homeTestPage.navigateToHtmlForm();
 	}
-	
-	@QAFTestStep(description="fill the html form and submit")
-	public void userFillHtmlFormAndSubmit(){
+
+	@QAFTestStep(description = "fill the html form and submit")
+	public void userFillHtmlFormAndSubmit() {
 		HtmlFormTestPage htmlFormTestPage = new HtmlFormTestPage();
 		htmlFormTestPage.fillHtmlForm();
 		htmlFormTestPage.clickSubmitOrder();
 	}
-	
-	
-	//@QAFTestStep(description="verifies form succesfull submittion")
-	public void userVerifiesFormSuccesfullSubmittion(){
+
+	@QAFTestStep(description = "verifies form succesfull submition")
+	public void userVerifiesFormSuccesfullSubmittion() {
 		HtmlFormBean htmlFormBean = new HtmlFormBean();
 		htmlFormBean.fillRandomData();
 		JSONObject jsonObject = new JSONObject();
@@ -52,7 +49,7 @@ public class WebServisesSteps extends RestWSTestCase {
 		jsonObject.put("custemail", htmlFormBean.getTxtCustEmail());
 		jsonObject.put("delivery", htmlFormBean.getTxtPrefDeliveryTime());
 		jsonObject.put("comments", htmlFormBean.getTxtDeliveryInstructions());
-	
+
 		getWebResource("/post").type(MediaType.APPLICATION_JSON).post(jsonObject.toString());
 		Response response = getResponse();
 		Validator.verifyThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
@@ -61,30 +58,148 @@ public class WebServisesSteps extends RestWSTestCase {
 		JsonObject formObject = responseBody.get("json").getAsJsonObject();
 		Reporter.log(formObject.toString());
 		Validator.verifyThat(formObject, Matchers.notNullValue());
-		Validator.verifyThat(formObject.get("custname"), Matchers.notNullValue());
-		Validator.verifyThat(formObject.get("custtel"), Matchers.notNullValue());
-		Validator.verifyThat(formObject.get("custemail"), Matchers.notNullValue());
-		Validator.verifyThat(formObject.get("delivery"), Matchers.notNullValue());
-		Validator.verifyThat(formObject.get("comments"), Matchers.notNullValue());
+		Validator.verifyThat(formObject.get("custname").toString(), Matchers.containsString(htmlFormBean.getTxtCustName()));
+		Validator.verifyThat(formObject.get("custtel").toString(), Matchers.containsString(htmlFormBean.getTxtTelePhone()));
+		Validator.verifyThat(formObject.get("custemail").toString(),  Matchers.containsString(htmlFormBean.getTxtCustEmail()));
+		Validator.verifyThat(formObject.get("delivery").toString(),  Matchers.containsString(htmlFormBean.getTxtPrefDeliveryTime()));
+		Validator.verifyThat(formObject.get("comments").toString(),  Matchers.containsString(htmlFormBean.getTxtDeliveryInstructions()));
+	}
+
+	
+	@QAFTestStep(description = "user want to verify get method response")
+	public void usereVrifiesGetMethodResponse() {
+		
 	}
 	
-	
-	
-	@Test
-	public void test(){
-		getClient().resource("https://now.httpbin.org/").type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
-		Response response = getResponse();
+	@QAFTestStep(description = "verifies the presence of headers and attributes in response body")
+	public void userVerifiesPresenceOfHeadersInResponseBody() {
+		JsonObject responseBody, headersObject;
+		Response response;
+		getWebResource("/get").type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		response = getResponse();
 		Validator.verifyThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
-		JsonObject responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
-		System.out.println(responseBody.toString());
-		JsonArray urlsArray = responseBody.get("urls").getAsJsonArray();
-		Reporter.log(urlsArray.toString());
-		Validator.verifyThat(urlsArray, Matchers.notNullValue());
-//		Validator.verifyThat(formObject.get("custname"), Matchers.notNullValue());
-//		Validator.verifyThat(formObject.get("custtel"), Matchers.notNullValue());
-//		Validator.verifyThat(formObject.get("custemail"), Matchers.notNullValue());
-//		Validator.verifyThat(formObject.get("delivery"), Matchers.notNullValue());
-//		Validator.verifyThat(formObject.get("comments"), Matchers.notNullValue());
-//		
+		responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
+
+		headersObject = responseBody.get("headers").getAsJsonObject();
+		if (Validator.verifyThat(headersObject, Matchers.notNullValue())) {
+			Validator.verifyThat(headersObject.get("Accept"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("User-Agent"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("Connection"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("Host"), Matchers.notNullValue());
+		}
+
+		Validator.verifyThat(responseBody.get("origin"), Matchers.notNullValue());
+		Validator.verifyThat(responseBody.get("url"), Matchers.notNullValue());
 	}
+	
+	@QAFTestStep(description = "verifies the current time formates in response body of {url}")
+	public void usereVrifiesGetMethodResponseOfUrl(String url) {
+		JsonObject responseBody, currentTimeObject;
+		JsonArray urlsArray;
+		Response response;
+		getClient().resource(url).type(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		response = getResponse();
+		Validator.verifyThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
+		responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
+
+		currentTimeObject = responseBody.get("now").getAsJsonObject();
+		if (Validator.verifyThat(currentTimeObject, Matchers.notNullValue())) {
+			Validator.verifyThat(currentTimeObject.get("epoch"), Matchers.notNullValue());
+			Validator.verifyThat(currentTimeObject.get("slang_date"), Matchers.notNullValue());
+			Validator.verifyThat(currentTimeObject.get("slang_time"), Matchers.notNullValue());
+			Validator.verifyThat(currentTimeObject.get("iso8601"), Matchers.notNullValue());
+			Validator.verifyThat(currentTimeObject.get("rfc2822"), Matchers.notNullValue());
+			Validator.verifyThat(currentTimeObject.get("rfc3339"), Matchers.notNullValue());
+		}
+
+		urlsArray = responseBody.get("urls").getAsJsonArray();
+		Reporter.log(urlsArray.toString());
+		if (urlsArray.size() > 0) {
+			Validator.verifyThat(urlsArray, Matchers.notNullValue());
+		}
+	}
+	
+	@QAFTestStep(description = "user want to verify http post method response")
+	public void usereVrifiesHttpPostMethodResponse() {
+		
+	}
+	
+	@QAFTestStep(description = "verifies the presence of headers and attributes in post method response body")
+	public void userVerifiesPresenceOfHeadersInPostMethodResponseBody() {
+		JsonObject responseBody, headersObject;
+		Response response;
+		getWebResource("/post").type(MediaType.APPLICATION_JSON).post();
+		response = getResponse();
+		Validator.verifyThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
+		responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
+
+		headersObject = responseBody.get("headers").getAsJsonObject();
+		if (Validator.verifyThat(headersObject, Matchers.notNullValue())) {
+			Validator.verifyThat(headersObject.get("Accept"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("User-Agent"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("Connection"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("Host"), Matchers.notNullValue());
+			Validator.verifyThat(headersObject.get("Content-Type"), Matchers.notNullValue());
+		}
+
+		Validator.verifyThat(responseBody.get("origin"), Matchers.notNullValue());
+		Validator.verifyThat(responseBody.get("url"), Matchers.notNullValue());
+	}
+	
+	@QAFTestStep(description = "user want to verify http delete method response")
+	public void usereVrifiesHttpDeleteMethodResponse() {
+		
+	}
+	
+	@QAFTestStep(description = "verifies the presence of headers and attributes in delete method response body")
+	public void userVerifiesPresenceOfHeadersInDeleteMethodResponseBody() {
+		JsonObject responseBody, headersObject;
+		Response response;
+		getWebResource("/delete").type(MediaType.APPLICATION_JSON).delete();
+		response = getResponse();
+		Validator.verifyThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
+		responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
+
+		headersObject = responseBody.get("headers").getAsJsonObject();
+		if (Validator.verifyThat(headersObject, Matchers.notNullValue())) {
+			Validator.verifyThat("Accept : ", headersObject.get("Accept"), Matchers.notNullValue());
+			Validator.verifyThat("User-Agent : ", headersObject.get("User-Agent"), Matchers.notNullValue());
+			Validator.verifyThat("Connection : ", headersObject.get("Connection"), Matchers.notNullValue());
+			Validator.verifyThat("Host : ", headersObject.get("Host"), Matchers.notNullValue());
+			Validator.verifyThat("Content-Type : ", headersObject.get("Content-Type"), Matchers.notNullValue());
+		}
+
+		Validator.verifyThat("origin : ", responseBody.get("origin"), Matchers.notNullValue());
+		Validator.verifyThat("url : ", responseBody.get("url"), Matchers.notNullValue());
+	}
+
+	@QAFTestStep(description = "user want to verify http put method response")
+	public void usereVrifiesHttpPutMethodResponse() {
+		
+	}
+	
+	@QAFTestStep(description = "verifies the presence of headers and attributes in put method response body")
+	public void userVerifiesPresenceOfHeadersInPutMethodResponseBody() {
+		JsonObject responseBody, headersObject;
+		Response response;
+		getWebResource("/put").type(MediaType.APPLICATION_JSON).put();
+		response = getResponse();
+		Validator.verifyThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
+		responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
+
+		headersObject = responseBody.get("headers").getAsJsonObject();
+		if (Validator.verifyThat(headersObject, Matchers.notNullValue())) {
+			Validator.verifyThat("Accept : ", headersObject.get("Accept"), Matchers.notNullValue());
+			Validator.verifyThat("User-Agent : ", headersObject.get("User-Agent"), Matchers.notNullValue());
+			Validator.verifyThat("Connection : ", headersObject.get("Connection"), Matchers.notNullValue());
+			Validator.verifyThat("Host : ", headersObject.get("Host"), Matchers.notNullValue());
+			Validator.verifyThat("Content-Type : ", headersObject.get("Content-Type"), Matchers.notNullValue());
+		}
+
+		Validator.verifyThat("data : ", responseBody.get("data"), Matchers.notNullValue());
+		Validator.verifyThat("form : ", responseBody.get("form"), Matchers.notNullValue());
+		Validator.verifyThat("origin : ", responseBody.get("origin"), Matchers.notNullValue());
+		Validator.verifyThat("url : ", responseBody.get("url"), Matchers.notNullValue());
+	}
+	
 }
